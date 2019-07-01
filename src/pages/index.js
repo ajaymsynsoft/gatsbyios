618f5deae9ed2bb249afdd214e89ec76f7c30e21
Ajay Mishra <ajaymishra@synsoftglobal.com>
2019-07-01 12:24:06 +0530

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class Test extends Component {
         url: null,
         pip: false,
         playing: true,
-        controls: true,
+        controls: false,
         legacyMode: true,
         light: false,
         volume: 0.8,
